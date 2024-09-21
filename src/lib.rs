be37564d5e4e495138ed55f9d4deab2726aa075c
Alexander Cyon <alex.cyon@gmail.com>
2024-09-21 19:51:37 +0200

    oars

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@
 type Result<T, E = String> = std::result::Result<T, E>;
 
 use indexmap::{IndexMap, IndexSet};
-use std::sync::Arc;
+use std::{
+    ops::Index,
+    sync::{Arc, Once},
+};
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct FactorSource {
@@ -15,7 +18,10 @@ pub struct FactorSource {
 pub struct FactorSourceIDFromHash;
 
 #[derive(Default, Clone, Debug, PartialEq, Eq, Hash)]
-pub struct FactorInstance;
+pub struct FactorInstance {
+    derivation_path: DerivationPath,
+    factor_source_id: FactorSourceIDFromHash,
+}
 
 #[derive(Default, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct NetworkID;
@@ -51,7 +57,26 @@ impl FactorSources {
 pub struct PartialDerivationRequests(IndexSet<PartialDerivationRequest>);
 
 #[derive(Default, Clone, Debug, PartialEq, Eq)]
-pub struct KnownTakenFactorInstances(pub IndexSet<FactorInstance>);
+pub struct KnownTakenFactorInstances {
+    pub recovered_unsecurified_accounts: IndexSet<UnsecurifiedAccount>,
+    pub recovered_securified_accounts: IndexSet<SecurifiedAccount>,
+}
+impl KnownTakenFactorInstances {
+    pub fn recovered_accounts(&self) -> IndexSet<Account> {
+        let mut accounts = IndexSet::new();
+        accounts.extend(
+            self.recovered_unsecurified_accounts
+                .iter()
+                .map(|a| Account::Unsecurified(a.clone())),
+        );
+        accounts.extend(
+            self.recovered_securified_accounts
+                .iter()
+                .map(|a| Account::Securified(a.clone())),
+        );
+        accounts
+    }
+}
 
 #[derive(Default, Clone, Debug, PartialEq, Eq)]
 pub struct ProbablyFreeFactorInstances(pub IndexSet<FactorInstance>);
@@ -61,14 +86,28 @@ pub struct FactorInstances(pub IndexSet<FactorInstance>);
 
 #[derive(Default, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct OnChainAnalyzer;
+impl OnChainAnalyzer {
+    pub fn new(gateway: Arc<dyn Gateway>) -> Self {
+        Self
+    }
+}
 
 #[derive(Default, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct ProfileAnalyzer;
 
 #[derive(Default, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Cache;
+impl Cache {
+    pub fn new(probably_free_factor_instances: ProbablyFreeFactorInstances) -> Self {
+        Self
+    }
+}
 
 pub trait DerivationInteractors {
+    fn call(&self);
+}
+
+pub trait Gateway {
     fn call(&self);
 }
 
@@ -77,6 +116,7 @@ pub struct DerivationsAndAnalysis {
     pub known_taken: KnownTakenFactorInstances,
     pub probably_free: ProbablyFreeFactorInstances,
 }
+impl DerivationsAndAnalysis {}
 
 #[derive(Default, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct DerivationPath;
@@ -96,14 +136,44 @@ impl KeysCollector {
     }
 }
 
-pub async fn _derive_many(
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct UnsecurifiedAccount(pub FactorInstance);
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct SecurifiedAccount(pub Vec<FactorInstance>);
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Account {
+    Unsecurified(UnsecurifiedAccount),
+    Securified(SecurifiedAccount),
+}
+
+#[derive(Default, Clone, Debug, PartialEq, Eq)]
+pub struct Profile {
+    pub factor_sources: FactorSources,
+    pub accounts: IndexSet<Account>,
+}
+impl Profile {
+    pub fn new(factor_sources: FactorSources, accounts: IndexSet<Account>) -> Self {
+        Self {
+            factor_sources,
+            accounts,
+        }
+    }
+}
+
+async fn _derive_many(
     factor_sources: FactorSources,
     partial_derivation_request: PartialDerivationRequests,
-    maybe_cache: Option<Cache>,
-    maybe_onchain_analyser: Option<OnChainAnalyzer>,
-    maybe_profile_analyser: Option<ProfileAnalyzer>,
+    maybe_cache: impl Into<Option<Cache>>,
+    maybe_onchain_analyser: impl Into<Option<OnChainAnalyzer>>,
+    maybe_profile_analyser: impl Into<Option<ProfileAnalyzer>>,
     derivation_interactors: Arc<dyn DerivationInteractors>,
 ) -> Result<DerivationsAndAnalysis> {
+    let maybe_cache = maybe_cache.into();
+    let maybe_onchain_analyser = maybe_onchain_analyser.into();
+    let maybe_profile_analyser = maybe_profile_analyser.into();
+
     assert!(factor_sources.can_derive(&partial_derivation_request));
     assert!(
         !(maybe_cache.is_none()
@@ -111,5 +181,35 @@ pub async fn _derive_many(
             && maybe_profile_analyser.is_none())
     );
 
+    // let mut instances = FactorInstances::default();
+    // let indices = ...;
+    // let requests = partial_derivation_request.materialize(indices);
+    // if let Some(ref cache) = maybe_cache {
+    //     let cached = cache.load(&requests).await?;
+    // }
+
     Ok(DerivationsAndAnalysis::default())
+}
+
+/// onboarding account recover scan
+pub async fn oars(
+    factor_sources: FactorSources,
+    derivation_interactors: Arc<dyn DerivationInteractors>,
+    gateway: Arc<dyn Gateway>,
+) -> Result<(Profile, Cache)> {
+    let analysis = _derive_many(
+        factor_sources.clone(),
+        PartialDerivationRequests::default(),
+        None,
+        OnChainAnalyzer::new(gateway),
+        None,
+        derivation_interactors,
+    )
+    .await?;
+
+    let cache = Cache::new(analysis.probably_free);
+    let accounts = analysis.known_taken.recovered_accounts();
+    let profile = Profile::new(factor_sources, accounts);
+
+    Ok((profile, cache))
 }
